add4867e47b9bb4ece083a9075acb40da22790bb
Oladipupo Zainab <oladipupozainab17@gmail.com>
2023-02-20 22:39:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let scienceSubjects = ['Physics', "Chemistry", 'Biology', 'Technical Drawing'];
+let socialscienceSubjects = ['Accounting', 'Commerce', 'Marketing', 'Geograph'];
+let artSubjects = ['Government', 'Economics', 'Literature', 'History'];
+let generalSubjects = ['English', 'Mathematics'];
+
+let bola = artSubjects;
+function subjects(){
+if ( bola == "science" ) {
+    console.log(scienceSubjects);
+} else if(bola == "socialscience"){
+    console.log(socialscienceSubjects);
+} else if (bola = "art"){
+    console.log(artSubjects);
+} else{
+    console.log(generalSubjects)
+}
+}
+
+
---
+++ read.md
@@ -0,0 +1,32 @@
+List the symbols for each of the below Javascript operators
+Arithmetic Operators +
+let x = 5;
+let y = 10;
+z = x + y;
+console.log(z);
+console.log(x + y);
+
+Assignment Operators =
+x = 5;
+f = 7;
+
+Comparison Operators > < >= <= === 
+ x = 5;
+ console.log( x > 5);
+ console.log(x < 9);
+
+Logical Operators and or not
+let high = true;
+let low = true;
+let x = low && high;
+console.log(x);
+let high = true;
+let low = true;
+let x = low || high;
+console.log(x);
+
+Bitwise Operators | > < ! >
+5 & 1;
+5 | 8
+
+i = 8
